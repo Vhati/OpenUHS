7d9d1ac8c99bd7084b79f6c237d02e78a2869c47
Vhati <tvtronix@yahoo.com>
2016-09-02 22:55:29 -0400

    Fixed no-arg (GUI) check in windows batch launcher

--- desktop-reader/skel/win/OpenUHS.bat
+++ desktop-reader/skel/win/OpenUHS.bat
@@ -8,7 +8,7 @@ java -cp "./lib/*" net.vhati.openuhs.desktopreader.UHSReaderMain %* || SET FAILE
 :die
 IF %FAILED%==1 (
   REM Pause after failure, but only if there were no args (GUI)
-  IF x%*x==xx PAUSE 1>&2
+  IF "%~1"=="" PAUSE 1>&2
 )
 ENDLOCAL
 EXIT /B %FAILED%
